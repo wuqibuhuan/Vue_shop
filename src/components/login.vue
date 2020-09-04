684c4271c103cf018174732c831c0f5f13d6f9f4
张东奇 <15937009100@163.com>
2020-09-04 10:57:29 +0800

    login-first commit

---
+++ src/components/login.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="login-conter">
+    <div class="login_box">
+      <div class="arvt_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form class="login_form">
+        <!-- 用户名 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-search"></el-input>
+        </el-form-item>
+
+        <!-- 密码 -->
+        <el-form-item>
+          <el-input prefix-icon="el-icon-search"></el-input>
+        </el-form-item>
+
+        <!-- 按钮 -->
+        <el-form-item class="btn">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style lang="less" scoped>
+.login-conter {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: aliceblue;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .arvt_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #ccc;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: aliceblue;
+    img {
+      width: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #fff;
+    }
+  }
+  .btn {
+      display: flex;
+      justify-content: flex-end;
+  }
+  .login_form {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 0px 20px;
+      box-sizing: border-box;
+  }
+}
+</style>
